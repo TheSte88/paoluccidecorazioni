a991b3e56f14f344cdc1467133e627bb5f3bb278
Elorvetti <manu.lorv@gmail.com>
2018-12-06 07:24:24 +0100

    V0.3
    Start gallery

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 "use strict";
 
 /* global tag*/
-var $navbar,$menuToggle,$menuList,
-$mainBox,$yPos,$hHeight,$titleBackground,$bufferImg,$areaBuffer,$windowWidth,$windowHeight;
+var $navbar,$menuToggle,$menuList, $btnGallery, 
+    $galleryImg, $imgMini;
 
 var app = {};
 app.name = "PAOLUCCI DECORAZIONI";
@@ -11,34 +11,83 @@ app.init = function ()
     $menuToggle =$(".btn-menu-toggle");
     $navbar = $(".navbar");
     $menuList = $(".menu-list");
-    $mainBox = $(".box");
-    $titleBackground = $(".title-background")
-    $areaBuffer = $(".area-buffer")
-    $bufferImg = $(".area.img-1")
+    $btnGallery = $(".title");
+    $galleryImg = $(".gallery-img");
+    $imgMini = $(".img-mini");
 
 
-    $menuToggle.on("click", app.toggleNavbar)
-
-
+    $menuToggle.on("click", app.toggleNavbar);
+    //$btnTexture.on("click", app.getJson().then(app.readImg));
+    $btnGallery.on("click",app.eventName);
 }
 
 // navbar
-app.toggleNavbar = function ()
-{    
+app.toggleNavbar = function(){
     if ($navbar.hasClass("active"))
         app.hideMenu();
     else
         app.showMenu();        
 };
-app.showMenu = function ()
-{
+app.showMenu = function(){
     $navbar.addClass("active");  
     $menuToggle.addClass("active");
 };
- app.hideMenu = function ()
-{
+ app.hideMenu = function(){
     $navbar.removeClass("active");
     $menuToggle.removeClass("active");
 };
 
+//Gallery
+app.eventName = function(e){
+    app.getData(e)
+
+    /*app.getJson().then(app.readJson);*/
+
+}
+app.getData = function(e){
+    var event = e.target.innerHTML;
+    event =event.toLowerCase();
+    return app.getJson()
+                .then(function(res){return res;})
+                .forEach(function(el){console.log(typeof(el));return el;})
+                .filter(function(item){console.log(item);})
+
+    /*
+    return app.getJson()
+                .then(function(res){res
+                .forEach(function(el){console.log(el[event])})})*/
+
+}
+app.getJson = function(){
+    return $.get("json/img.json");
+}
+app.readJson = function(res){
+    console.log({result:res})
+    res.forEach(function(el){console.log({element:el})})
+}
+
+app.image = function(e){
+    return app.getJson().filter(function(item){return item === e.target.innerHTML})
+}
+app.filterImg = function(item){
+    return item === e.target.innerHTML;
+}
+
+
+app.showGallery = function(){
+    if ($galleryImg.hasClass("active"))
+    {
+        app.hideGallery();
+    }
+    else
+    {
+        app.showGallery();
+    }
+}
+app.hideGallery = function(){
+    $galleryImg.removeClass("active");
+}
+app.showGallery = function(){
+    $galleryImg.addClass("active");
+}
 
